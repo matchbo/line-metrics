1a912275852a3eca4b0f3914920e6638762c2a31
Matt <matchbo@users.noreply.github.com>
2023-09-27 14:06:15 +1000

    First commit of line_metrics.sh
    This script takes a filename as an argument and returns either the lines greater than a max line length, or the lines sorted by line length (highest number of characters, excluding newline).
    
    It takes two possible arguments:
    -n: The max number of lines to return
    -l, --length: The max line length. Any lines longer than this length are returned.

---
+++ line_metrics.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# line_metrics: Analyse lines in a text file, sorting them by character count
+
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -l|--length) length="$2"; shift; shift ;;
+        -n) num_lines="$2"; shift; shift ;;
+        *) filename="$1"; shift ;;
+    esac
+done
+
+# Validate input file
+if [[ ! -f $filename ]]; then
+    echo "Error: File '$filename' not found."
+    exit 1
+fi
+
+# Main logic to process each line and filter out those longer than $length
+process_lines() {  # creates the process_lines function
+    local counter=1
+    while IFS= read -r line; do
+        char_count=$(echo -n "$line" | wc -c | awk '{print $1}')
+        
+        # If $length is defined, filter out only the lines which are greater than $length long
+        if [[ ! -n $length || $char_count -gt $length ]]; then
+            echo "$counter,$char_count:$line"
+        fi    
+        (( counter++ ))
+    done < "$filename"
+}
+
+# Process and sort the file
+lines=$(process_lines | sort -t',' -k2,2nr)
+
+# Filter out just the top $num_lines in terms of length
+if [[ -n $num_lines ]]; then
+    echo "$lines" | head -n "$num_lines"
+else
+   echo "$lines" 
+fi
